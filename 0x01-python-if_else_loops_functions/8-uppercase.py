9523b312285ce7955efbdd3d8019a93192bcdfb4
Dexterski007 <mbouj007@gmail.com>
2024-01-02 14:35:47 -0800

    indent

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) <= ord('z') and ord(c) >= ord('a'):
+        return True
+    else:
+        return False
+
+def uppercase(str):
+    for c in str:
+        print("{:c}" .format(
+              ord(c) if not islower(c) else ord(c) - 32), end="")
+    print(" ")
