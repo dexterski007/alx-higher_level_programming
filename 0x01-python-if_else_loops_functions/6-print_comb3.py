b11b7aeae2eb7aa962711775cb8a6bac947cd196
Dexterski007 <mbouj007@gmail.com>
2024-01-02 14:16:13 -0800

    b

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(a, 10):
+        if a < b:
+            print("{:d}{:d}".format(a, b),
+                end="\n" if a == 8 and b == 9 else ", ")
