6a2cd9b7bdfe5108d3762b849145b0105ed3429d
Dexterski007 <mbouj007@gmail.com>
2024-01-04 05:46:35 -0800

    ind

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-from calculator_1 import add, sub, mul, div
+    import sys
+    from calculator_1 import add, sub, mul, div
     args = sys.argv[1:]
     totarg = len(args)
     if totarg != 3:
