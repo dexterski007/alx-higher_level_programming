446530581eef639b4ff5873d4f50ce021dbc57cf
Dexterski007 <mbouj007@gmail.com>
2024-01-04 06:22:05 -0800

    mul

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif args[1] == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif args[1] == '*':
+    elif args[1] == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
