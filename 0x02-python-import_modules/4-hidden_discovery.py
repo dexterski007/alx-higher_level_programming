43a7792a5c3d6bfef5f1a5f94919d2cbcdc02158
Dexterski007 <mbouj007@gmail.com>
2024-01-04 05:28:16 -0800

    pu

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import hidden_4
     listd = dir(hidden_4)
-    l = len(listd)
-    for i in range(l):
+    leng = len(listd)
+    for i in range(leng):
         txt = listd[i]
         if not txt.startswith("__"):
             print("{}".format(txt))
