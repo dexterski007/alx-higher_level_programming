678bd5bdf9adbca17a1f79597e0cf6bd60beb55b
Dexterski007 <mbouj007@gmail.com>
2024-01-07 03:50:01 -0800

    chek

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,34 +14,26 @@ int is_palindrome(listint_t **head)
 {
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	size_t len = 0, halflen = 0, i = 0;
-	listint_t *current = *head, *prev = NULL, *nextp;
-	listint_t *first_half = NULL, *second_half = NULL;
-
-	while (current != NULL)
-	{
-		len++;
-		current = current->next;
-	}
-	halflen = len / 2;
-	current = *head;
-	for (i = 0; i < halflen; i++)
-	{
-		nextp = current->next;
-		current->next = prev;
-		prev = current;
-		current = nextp;
-	}
-	*head = prev;
-	first_half = *head;
-	second_half = current;
-	for (i = 0; i < halflen; i++)
-	{
-		if (first_half->n != second_half->n)
-			return (0);
-		first_half = first_half->next;
-		second_half = second_half->next;
-	}
-	return (1);
+	return (aux_palind(head, *head));
 }
 
+
+/**
+ * aux_palind - check the code for
+ *
+ * @head: head
+ *
+ * @end: end list
+ */
+
+int aux_palind(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return(1);
+	}
+	return (0);
+}
