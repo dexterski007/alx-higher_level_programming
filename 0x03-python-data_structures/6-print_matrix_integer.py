83b06eff2ea5303624a626f7f8e01187535420a4
Dexterski007 <mbouj007@gmail.com>
2024-01-07 01:03:11 -0800

    non

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix == "":
+        return None
     for i in matrix:
         for j in i:
             print("{:d}".format(j), end=" ")
