d045bf9fda9343d03bbc7e92f2323b172b7860ff
Dexterski007 <mbouj007@gmail.com>
2024-01-07 01:49:49 -0800

    pp

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list == "":
+        return None
+    value = 0
+    for i in my_list:
+        if i > value:
+            value = i
+    return value
