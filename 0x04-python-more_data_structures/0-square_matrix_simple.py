94415d9141db36736fefbca1db860cd486ab2eeb
Dexterski007 <mbouj007@gmail.com>
2024-01-10 10:07:54 -0800

    indent

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
-            j = list(map(lambda x: x**2, i))
-            new_matrix.append(j)
+        j = list(map(lambda x: x**2, i))
+        new_matrix.append(j)
     return new_matrix
