5b6ffc82168c718439ab340acc97adf6437f21f5
Dexterski007 <mbouj007@gmail.com>
2024-01-10 11:23:35 -0800

    none

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    bestscore = 0
+    bestkey = None
+    for key, value in a_dictionary.items():
+        if value > bestscore:
+            bestscore = value
+            bestkey = key
+    return bestkey
