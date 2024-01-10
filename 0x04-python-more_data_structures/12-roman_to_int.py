6369f0e258112a67337e789c25f09f917859204d
Dexterski007 <mbouj007@gmail.com>
2024-01-10 11:54:29 -0800

    hey

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    romandict = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    numb = 0
+    for i in range(len(roman_string)):
+        if i < len(roman_string) - 1 and romandict[roman_string[i]] \
+        < romandict[roman_string[i + 1]]:
+            numb -= romandict[roman_string[i]]
+        else:
+            numb += romandict[roman_string[i]]
+
+    return numb
