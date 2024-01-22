735364b891488e5799e2097d754723441ae53a11
Dexterski007 <mbouj007@gmail.com>
2024-01-22 12:32:01 -0800

    magic

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1,3)
+        try:
+            if i > a:
+                raise IndexError("Too far")
+            result += a ** b / i
+        except IndexError:
+            result = a + b
+            break
+        finally:
+            return result
