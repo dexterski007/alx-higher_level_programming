6928e716890362360d9b1aa135cd6d42b048c4f7
Dexterski007 <mbouj007@gmail.com>
2024-01-22 11:06:32 -0800

    whie

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     length = []
-    
+
     for i in range(list_length):
         try:
             length.append(my_list_1[i] / my_list_2[i])
