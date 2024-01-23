82bafa791b975ca39a9b675ff6aa1305d1e2280d
Dexterski007 <mbouj007@gmail.com>
2024-01-23 13:45:26 -0800

    magicsyst

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import math
+""" magic - is refining a potter """
+
+
+class MagicClass:
+    """ this is magic """
+    def __init__(self, radius=None):
+        if not isinstance(radius, (int, float)):
+            raise TypeError(radius must be a number)
+        self.radius = radius
+    
+    def area(self):
+        return self.radius ** 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
