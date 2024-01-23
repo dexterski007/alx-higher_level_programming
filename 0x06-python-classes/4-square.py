3af962410180670aa56b38490b9451292f309778
Dexterski007 <mbouj007@gmail.com>
2024-01-23 06:41:19 -0800

    pythonic

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,11 +7,7 @@ class Square:
     """ this is a square """
     def __init__(self, size=0):
         """ constructor """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     def area(self):
         """ area instance """
