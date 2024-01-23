fdd9ace009ddb5fc8a069e69564b3e85d5a80c63
Dexterski007 <mbouj007@gmail.com>
2024-01-23 13:52:39 -0800

    hey

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import math
 """ magic - is refining a potter """
+
+import math
 
 
 class MagicClass:
@@ -11,10 +12,10 @@ class MagicClass:
         if not isinstance(radius, (int, float)):
             raise TypeError("radius must be a number")
         self.__radius = radius
-    
+
     def area(self):
         """ return """
-        return self.__radius ** 2 * math.pi
+        return (self.__radius ** 2 * math.pi)
         """ return"""
     def circumference(self):
-        return 2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)
