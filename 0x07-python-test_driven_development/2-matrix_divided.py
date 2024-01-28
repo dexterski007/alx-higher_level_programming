9865924e82766d97b046e890f9ef040376705621
Dexterski007 <mbouj007@gmail.com>
2024-01-28 11:03:14 -0800

    hey

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,21 +4,22 @@
 
 
 def matrix_divided(matrix, div):
-    """ 
+    """
     This module divides a matrix by a dividor
-    Args: 
+    Args:
         matrix: the matrix
         div: neo the divider
     Returns:  the divided matrix
     Raises:
 
     """
-    
-    if (not isinstance(matrix, list) or len(matrix) == 0 or \
-        not all(isinstance(line, list) for line in matrix) or \
-        not all(isinstance(i, (int, float)) for i in [n for line\
-            in matrix for n in line])):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if (not isinstance(matrix, list) or len(matrix) == 0 or
+        not all(isinstance(line, list) for line in matrix) or
+        not all(isinstance(i, (int, float)) for i in [n for line
+                in matrix for n in line])):
+        raise TypeError("matrix must be a matrix\
+        (list of lists) of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
@@ -26,10 +27,9 @@ def matrix_divided(matrix, div):
     for line in matrix:
         if len(line) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-       
+
     matrix2 = [[round(i / div, 2) for i in line] for line in matrix]
     return matrix2
-
 
 
 if __name__ == "__main__":
