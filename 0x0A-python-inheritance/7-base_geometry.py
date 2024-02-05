7d10db6b5cdbbfc0ad996db4600ca65fe00f3d8c
Dexterski007 <mbouj007@gmail.com>
2024-02-05 04:51:55 -0800

    hey

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" test classes """
+
+
+class BaseGeometry:
+    """ geo v2 class """
+    def area(self):
+        """ area not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ check int """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
