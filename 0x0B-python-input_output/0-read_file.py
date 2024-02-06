a1099e36165191cfc60573615276298f0b2f19b7
Dexterski007 <mbouj007@gmail.com>
2024-02-06 11:26:12 -0800

    hey

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ this is a module for reaading files """
 
+
 def read_file(filename=""):
-	""" read files """
-	with open(filename, 'r') as file:
-		file_contents = file.read()
-		print(file_contents, end='')
+    """ read files """
+    with open(filename, 'r') as file:
+        file_contents = file.read()
+        print(file_contents, end='')
