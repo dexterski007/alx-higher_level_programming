9b0a92378eb7e537cf1ed0efb02b9f1e4f676423
Dexterski007 <mbouj007@gmail.com>
2024-02-06 13:35:29 -0800

    hey

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" this is a class for reaading files """
+
+
+class Student:
+    """ class student """
+    def __init__(self, first_name, last_name, age):
+        """ instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ public method """
+        try:
+            for attribute in attrs:
+                if type(attribute) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        new_dic = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dic[key] = value
+        return new_dic
+
+        return self.__dict__
