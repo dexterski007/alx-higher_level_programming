1693f6d813714a2b9706c2566b77e272fcbead79
Dexterski007 <mbouj007@gmail.com>
2024-02-06 12:46:53 -0800

    hey

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,10 +4,7 @@
 
 import json
 
+
 def class_to_json(obj):
     """ class json """
-    attr = {}
-    for attr_name, attr_value in obj.__dict__.items():
-        if isinstance(attr_value, (list, dict, str, bool, int)):
-            attr[attr_name] = attr_value
-    return attr
+    return obj.__dict__
