a2198861fec722e9b31345e13a8b9928321d2410
Dexterski007 <mbouj007@gmail.com>
2024-02-06 13:25:09 -0800

    lld

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -12,8 +12,4 @@ class Student:
 
     def to_json(self):
         """ public method """
-        return {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'age': self.age
-        }
+        return self.__dict__
