9f522efa88372c83f2ecaca937cafb22162b0639
Dexterski007 <mbouj007@gmail.com>
2024-03-12 10:31:34 -0700

    converting to utf8

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,3 @@
--- list all records with exceptions
-SELECT score, name
-FROM second_table
-WHERE name != ''
-ORDER BY score DESC;
+-- UTF8 converter script
+USE hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
