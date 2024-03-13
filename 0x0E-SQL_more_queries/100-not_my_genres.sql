9ddc268761f8d1b631e8a1c6a50d9e1aad01ce32
Dexterski007 <mbouj007@gmail.com>
2024-03-13 13:23:33 -0700

    tv genres

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- list shows and genres
+SELECT name
+FROM tv_genres
+WHERE name not IN
+(SELECT name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name ASC;
