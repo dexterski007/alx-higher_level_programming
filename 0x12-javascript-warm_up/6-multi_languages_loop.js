3ecaed640adf334c69e5137c4f006f3d96c5fc05
Dexterski007 <mbouj007@gmail.com>
2024-04-08 11:12:42 -0700

    cap

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
-for (const i of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(i);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(array[i]);
 }
