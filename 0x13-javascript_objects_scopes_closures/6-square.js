64f9a222affb7e6a5518c255a42cc5397779592c
Dexterski007 <mbouj007@gmail.com>
2024-04-09 06:29:00 -0700

    inherit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Square2 = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Square2 {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
