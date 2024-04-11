7ac17f30a73dfb251946b8952899f585ffacb532
Dexterski007 <mbouj007@gmail.com>
2024-04-11 14:13:07 -0700

    delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" run things using sqlalchemy """
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def db_run(username, password, db_name):
+    """ run from sqlalchemy"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.bind = engine
+    session = sessionmaker(bind=engine)()
+    instance = session.query(State).filter(State.name.like('%a%'))
+    for obj in instance:
+        session.delete(obj)
+    session.commit()
+
+
+if __name__ == '__main__':
+    db_run(sys.argv[1], sys.argv[2], sys.argv[3])
