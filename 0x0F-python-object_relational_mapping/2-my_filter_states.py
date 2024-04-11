fbb382f6e289ac5ebd05424812d0688dcc2031e4
Dexterski007 <mbouj007@gmail.com>
2024-04-11 11:41:26 -0700

    statename

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" list states from db """
+import MySQLdb
+import sys
+
+
+def connect_db(username, password, db_name, state_name):
+    """ connect function """
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}'\
+                    ORDER BY id ASC".format(state_name))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    connect_db(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
