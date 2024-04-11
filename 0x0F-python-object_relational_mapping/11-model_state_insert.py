e86e7f07f7b205d3cfdc34f396085c43f0859e30
Dexterski007 <mbouj007@gmail.com>
2024-04-11 13:52:09 -0700

    deprecated

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" run things using sqlalchemy """
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def db_run(username, password, db_name):
+    """ run from sqlalchemy"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.bind = engine
+    session = sessionmaker(bind=engine)()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    
+    print(new_state.id)
+
+
+if __name__ == '__main__':
+    db_run(sys.argv[1], sys.argv[2], sys.argv[3])
