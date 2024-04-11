7b786beaa70ce5bf735f5f026d997bc4a04de646
Dexterski007 <mbouj007@gmail.com>
2024-04-11 09:51:43 -0700

    firs_tocuyh

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" list states from db """
+import MySQLdb
+import sys
+
+
+def connect_db(username, password, db_name):
+    """ connect function """
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    connect_db(sys.argv[1], sys.argv[2], sys.argv[3])
