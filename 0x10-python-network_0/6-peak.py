95c4e39b3ee49bf2b384027ee0a5d766a37e988e
Dexterski007 <mbouj007@gmail.com>
2024-04-25 11:01:46 -0700

    pycode

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,17 +3,17 @@
 
 
 def find_peak(list_of_integers):
-	""" fucntion to find peak """
-	if list_of_integers is None or len(list_of_integers) == 0:
-		return None
-	if len(list_of_integers) == 1:
-		return list_of_integers
-	max_nb = list_of_integers[0]
-	nb = int(len(list_of_integers) / 2)
-	find_peak(list_of_integers[:nb - 1])
-	find_peak(list_of_integers[nb:])
-	
-	for i in list_of_integers:
-		if max_nb < i:
-			max_nb = i
-	return max_nb
+    """ fucntion to find peak """
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers
+    max_nb = list_of_integers[0]
+    nb = int(len(list_of_integers) / 2)
+    find_peak(list_of_integers[:nb - 1])
+    find_peak(list_of_integers[nb:])
+
+    for i in list_of_integers:
+        if max_nb < i:
+            max_nb = i
+    return max_nb
