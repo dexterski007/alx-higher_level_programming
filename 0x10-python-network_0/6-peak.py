525d5b9315779d0816d659a25ee2151bc2dd2214
Dexterski007 <mbouj007@gmail.com>
2024-04-25 10:59:37 -0700

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" function for big o algorithms of sorting """
+
+
+def find_peak(list_of_integers):
+	""" fucntion to find peak """
+	if list_of_integers is None or len(list_of_integers) == 0:
+		return None
+	if len(list_of_integers) == 1:
+		return list_of_integers
+	max_nb = list_of_integers[0]
+	nb = int(len(list_of_integers) / 2)
+	find_peak(list_of_integers[:nb - 1])
+	find_peak(list_of_integers[nb:])
+	
+	for i in list_of_integers:
+		if max_nb < i:
+			max_nb = i
+	return max_nb
