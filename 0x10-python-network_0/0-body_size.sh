652966b3f68e7acf62eea34b93f853e5e8364241
Dexterski007 <mbouj007@gmail.com>
2024-04-25 06:05:17 -0700

    curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # curling body size
-
-output=$(curl -sI "$1" | grep -i "Content-Length" | awk 'print $2')
-
-echo "$output"
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
