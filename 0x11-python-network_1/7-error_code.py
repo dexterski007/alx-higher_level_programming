9d8c574c8c23f8c2b75667e7945714af6d651a6c
Dexterski007 <mbouj007@gmail.com>
2024-04-26 14:35:46 -0700

    pyco

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,8 @@ import sys
 if __name__ == "__main__":
 
     url = sys.argv[1]
-    
     resp = requests.get(url)
-    if resp.status_code >= 400:
-        print("Error code:", resp.status_code)
-    else:
+    if resp.status_code < 400:
         print(resp.text)
+    else:
+        print("Error code:", resp.status_code)
