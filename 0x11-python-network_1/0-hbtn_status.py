9133b4fa0dbfdcff449c55fbc899e9087d71e27a
Dexterski007 <mbouj007@gmail.com>
2024-04-26 12:12:49 -0700

    test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,15 @@
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode('utf-8')
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
 
-print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 content:", utf8_content)
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", utf8_content)
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,10 +2,14 @@
 """ status using urllib """
 
 
+import urllib.parse
 import urllib.request
 import sys
 
 url = sys.argv[1]
+mail = sys.argv[2]
+
+data
 
 with urllib.request.urlopen(url) as response:
     header = response.getheader('X-Request-Id')
