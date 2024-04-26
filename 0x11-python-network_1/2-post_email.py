cd6f72bbe920d850c8fddacef12831fa92154f6e
Dexterski007 <mbouj007@gmail.com>
2024-04-26 12:02:23 -0700

    yes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,6 @@ import sys
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
-    header = response.headers.get("X-Request-Id")
+    header = response.getheader('X-Request-Id')
 
 print(header)
