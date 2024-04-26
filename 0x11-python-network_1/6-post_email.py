e23c5c675cc448c8ffd3d0f0d345a4982766ef8c
Dexterski007 <mbouj007@gmail.com>
2024-04-26 14:06:27 -0700

    hey

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     mail = sys.argv[2]
+    data = {'email': mail}
 
-    resp = requests.post(url, mail)
+    resp = requests.post(url, data)
 
     print(resp.text)
