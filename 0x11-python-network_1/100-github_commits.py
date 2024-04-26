4d00e26bfa9b7524625fbf214bae0196a65b93c2
Dexterski007 <mbouj007@gmail.com>
2024-04-26 15:23:14 -0700

    hey

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
 
     req = requests.get(url)
-    if req.status_code == 200:
-        commits = req.json()
+    commits = req.json()
+    try:
         for commit in commits[:10]:
             sha = commit['sha']
             author = commit['commit']['author']['name']
             print("{}: {}".format(sha, author))
-    else:
-        print("Error")
+    except IndexError:
+        pass
