11cba384a505ba03ee4775bd575afe0ca4eb4751
Dexterski007 <mbouj007@gmail.com>
2024-04-26 12:22:54 -0700

    fi

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     mail = sys.argv[2]
 
-    data = urllib.parse.urlencode({'email: mail'}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': mail}).encode('utf-8')
 
     with urllib.request.urlopen(url, data=data) as response:
         resp = response.read().decode('utf-8')
