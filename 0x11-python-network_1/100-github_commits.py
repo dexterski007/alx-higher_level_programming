a8b4db4df06d66650738432f11f7e7c3a97baf80
Dexterski007 <mbouj007@gmail.com>
2024-04-26 15:27:50 -0700

    owner

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,17 +6,17 @@ import sys
 
 if __name__ == "__main__":
 
-    owner = sys.argv[1]
-    repo = sys.argv[2]
+    owner = sys.argv[2]
+    repo = sys.argv[1]
 
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
 
     req = requests.get(url)
-    commits = req.json()
-    try:
-        for i in range(10):
-            sha = commits[i].get("sha")
-            author = commits[i].get("commit").get("author").get("name")
+    if req.status_code == 200:
+        commits = req.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author = commit['commit']['author']['name']
             print("{}: {}".format(sha, author))
-    except IndexError:
+    else:
         pass
