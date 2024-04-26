ef5610e355f290b87f4492c4edfcc13dfe5c6b6f
Dexterski007 <mbouj007@gmail.com>
2024-04-26 14:45:58 -0700

    hey

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" status using requests """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = 'http://0.0.0.0:5000/search_user'
+    req = sys.argv[1]
+    if req is None:
+        req = ""
+    payload = {'q': req}
+    resp = requests.post(url, data=req)
+    try:
+        jsondata = resp.json()
+        if jsondata:
+            print("[{}] {}".format(jsondata.get('id'), jsondata.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
